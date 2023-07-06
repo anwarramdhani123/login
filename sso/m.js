ce77bc6d11fb012af6110345c18ad2b7de1d4b87
anwarramdhani123 <88753181+anwarramdhani123@users.noreply.github.com>
2023-07-06 11:30:20 +0700

    Update m.js

--- sso/m.js
+++ sso/m.js
@@ -1,22 +1,32 @@
 // Firebase configuration
 var firebaseConfig = {
-  apiKey: "AIzaSyDwLz4xTWaJ-3vVkAm4-14MZuNtxmTea7A",
-    authDomain: "anwr-login.firebaseapp.com",
-    projectId: "anwr-login",
-    storageBucket: "anwr-login.appspot.com",
-    messagingSenderId: "686366789818",
-    appId: "1:686366789818:web:8e4322533cf1c18b687283"
+  // Konfigurasi Firebase Anda di sini
 };
 
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
+
+// Cek status login setiap kali halaman dimuat
+firebase.auth().onAuthStateChanged(function(user) {
+  if (!user) {
+    // Jika pengguna belum login, redirect ke halaman login
+    redirectToLogin();
+  }
+});
+
+// Simpan URL halaman asal saat mengarahkan pengguna ke halaman login
+function redirectToLogin() {
+  var currentUrl = window.location.href;
+  sessionStorage.setItem("redirectUrl", currentUrl);
+  window.location.href = "/sso/login22.html";
+}
 
 // Logout function
 function logout() {
   firebase.auth().signOut()
     .then(function() {
       // Redirect to login page after successful logout
-      window.location.href = "/login.html";
+      redirectToLogin();
     })
     .catch(function(error) {
       alert("Error: " + error.message);
