bae3aa4a1bc82eb0b506e369b08b621feef512e7
anwarramdhani123 <88753181+anwarramdhani123@users.noreply.github.com>
2023-07-06 17:20:27 +0700

    Update login.js

--- sso2/login.js
+++ sso2/login.js
@@ -1,3 +1,16 @@
+// Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyDwLz4xTWaJ-3vVkAm4-14MZuNtxmTea7A",
+    authDomain: "anwr-login.firebaseapp.com",
+    projectId: "anwr-login",
+    storageBucket: "anwr-login.appspot.com",
+    messagingSenderId: "686366789818",
+    appId: "1:686366789818:web:8e4322533cf1c18b687283"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
 // Fungsi Login dengan Google
 function loginWithGoogle() {
   var provider = new firebase.auth.GoogleAuthProvider();
