c1fc613feffcc591528f999d2f06706e29acd8ff
anwarramdhani123 <88753181+anwarramdhani123@users.noreply.github.com>
2023-07-06 11:14:24 +0700

    Update m.js

--- sso/m.js
+++ sso/m.js
@@ -1,6 +1,11 @@
 // Firebase configuration
 var firebaseConfig = {
-  // Konfigurasi Firebase Anda di sini
+  apiKey: "AIzaSyDwLz4xTWaJ-3vVkAm4-14MZuNtxmTea7A",
+    authDomain: "anwr-login.firebaseapp.com",
+    projectId: "anwr-login",
+    storageBucket: "anwr-login.appspot.com",
+    messagingSenderId: "686366789818",
+    appId: "1:686366789818:web:8e4322533cf1c18b687283"
 };
 
 // Initialize Firebase
